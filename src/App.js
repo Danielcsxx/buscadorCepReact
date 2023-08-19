3f71d7287363e771441890d42ef952e0a6df4f6b
Danielcsxx <daniel.csxx@gmail.com>
2023-08-18 22:28:27 -0300

    Add - Regex + update

--- src/App.js
+++ src/App.js
@@ -12,36 +12,42 @@ function App() {
   const [cep, setCep] = useState({});
 
   async function handleSearch() {
+    const cleanedInput = input.replace(/[^\d]/g, '');
 
-    if(input === '') {
-      toast.warning("CEP inválido.. tente novamente");
+    if (!cleanedInput.match(/^\d{8}$/)) {
+      toast.warning("CEP inválido. Insira um CEP válido com 8 dígitos.");
       return;
-    } try {
-      const response = await api.get(`${input}/json`);
+    }
+
+    try {
+      const response = await api.get(`${cleanedInput}/json`);
+      if (response.data.erro) {
+        toast.error("CEP não encontrado. Verifique o CEP informado.");
+        return;
+      }
+
       setCep(response.data);
       setInput("");
-      toast.success("Aqui está seu endereço!")
-
-    } catch {
-      toast.error("Ops.. Erro ao buscar cep.");
+      toast.success("Aqui está seu endereço!");
+    } catch (error) {
+      toast.error("Ops.. Erro ao buscar cep. Verifique se o CEP é válido.");
       setInput("");
     }
   }
-
   return (
     <div className="container">
       <h1 className="title"><a href="/">Buscar meu CEP</a></h1>
       <div className="containerInput">
-
         <input
-          type='text'
+          type="text"
           placeholder="Digite aqui o seu CEP"
-          size="40"
+          size={40}
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          maxLength="10"
         />
         <button className="buttonSearch" onClick={handleSearch}>
-          <FiSearch size={25} color="#FFF" />
+          <FiSearch size={22} color="#FFF" />
         </button>
       </div>
 
